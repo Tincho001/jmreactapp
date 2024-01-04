b0dc019a5a9f1e49192f109213d3ea5c9d64a9d9
Martin <tikoloko22@gmail.com>
2024-01-04 19:58:05 -0300

    pr cel

--- src/components/Categorias/Categoria.jsx
+++ src/components/Categorias/Categoria.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, ButtonGroup, Menu, MenuList, MenuItem } from '@chakra-ui/react';
+import { Button, ButtonGroup,} from '@chakra-ui/react';
 import { selectCategory } from '../../redux/categories/categoriesSlice';
 
 const Categoria = ({ title, category }) => {
@@ -8,25 +8,19 @@ const Categoria = ({ title, category }) => {
   const dispatch = useDispatch();
 
   return (
-    <Menu>
-      <ButtonGroup
-        as={Button}
-        mt={{ base: 2, md: 0 }}
-        ml={{ sm: 2, md: 0 }}
-        width={{ md: 60 }}
+   
+    <ButtonGroup variant='outline'  display={{ md: "flex"}}>
+      <Button 
+    mt={{ base: 4, md: 0 }} ml={{ sm: 6 }}
+      width={{ md: 60 }}
         colorScheme='whiteAlpha'
         isActive={category === selectedCategory}
-        isDisabled={false}
-        display={{ base: 'none', md: 'block' }}  
+        isDisabled={false}  
+        onClick={() => dispatch(selectCategory(category))}
       >
         {title}
-      </ButtonGroup>
-      <MenuList>
-        <MenuItem onClick={() => dispatch(selectCategory(category))}>
-          {title}
-        </MenuItem>
-      </MenuList>
-    </Menu>
+      </Button>
+    </ButtonGroup> 
   );
 };
 
